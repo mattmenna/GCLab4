33c3083a5fc45104e26588239f7b9835f57cf555
Matthew Menna <mattmenna@gmail.com>
2017-06-26 14:38:23 -0400

    Do While loop added for continuation of program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,14 +13,25 @@ public class Main {
         // write your code here
         System.out.println("Welcome to the factorial calculator");
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter an integer");
-        int uInput = scnr.nextInt();
-        System.out.println(factorial(uInput));
+        char cont;
+        do {
+            System.out.println("Enter an integer that's greater than 0 but less than 10");
+            int uInput = scnr.nextInt();
+            if (uInput>10 || uInput<0){
+                System.out.println("Input is not in range");
+                return;
+            }
+
+            System.out.println(factorial(uInput));
+            System.out.println("Do you want to continue? Please use y or n");
+            cont = scnr.next().charAt(0);
+        }
+        while (cont != 'n');
     }
 
-    private static long factorial(int uInput){
+    private static long factorial(int uInput) {
         int numFact = 1;
-        for (int i =1; i <=uInput; i++){
+        for (int i = 1; i <= uInput; i++) {
             numFact *= i;
         }
         return numFact;
